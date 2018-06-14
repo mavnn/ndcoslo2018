16b652eb9881552c64c33bc9c74246089bee81ca
mavnn <michael@mavnn.co.uk>
2018-06-14 23:48:14 +0200

    Without "null within model" or "invalid key" handling

--- Rendering.Implementation/Renderer.fs
+++ Rendering.Implementation/Renderer.fs
@@ -7,8 +7,6 @@ open System.Text.RegularExpressions
 type RenderingError =
     | NullModel
     | NullContent
-    | NullWithinModel
-    | InvalidKeySupplied
 
 let private renderTextLogic model content =
     let fold str (key, value : string) =
@@ -18,28 +16,12 @@ let private renderTextLogic model content =
     |> Seq.map (fun (kv : KeyValuePair<_,_>) -> kv.Key, kv.Value)
     |> Seq.fold fold content
 
-let private isNullWithinModel model =
-    model
-    |> Seq.exists (fun (kv : KeyValuePair<_,_>) -> isNull kv.Key || isNull kv.Value)
-
-let private allKeysValid model =
-    let isValidKey k =
-        not (String.IsNullOrWhiteSpace k)
-        && Char.IsLetter k.[0]
-        && (k.[1..] |> Seq.forall Char.IsLetterOrDigit)
-    model
-    |> Seq.forall (fun (kv : KeyValuePair<_,_>) -> isValidKey kv.Key)
-
 let renderText model content =
     match model, content with
     | m, _ when isNull m ->
         Error NullModel
     | _, c when isNull c ->
         Error NullContent
-    | m, _ when isNullWithinModel m ->
-        Error NullWithinModel
-    | m, _ when not (allKeysValid m) ->
-        Error InvalidKeySupplied
     | _ ->
         renderTextLogic model content
         |> Ok
